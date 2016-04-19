b55f52ec035e34b113b551fb1e7ba03ca4417e16
Alex Deddo <alex.deddo@daqri.com>
2016-04-18 20:07:41 -0700

    Clean 'extra package' logic and commenting

--- diffapt.sh
+++ diffapt.sh
@@ -11,6 +11,10 @@ tput sgr0
 F_CREATE=0
 [[ "$1" == "--create" ]] && F_CREATE=1
 
+## Establish as empty tempfile
+PKG_TMP=/tmp/.diffapt_tmp
+>$PKG_TMP
+
 function create_new_list() {
 	dpkg --list | tail -n+6 | while read i; do
 		PKG_STR=`echo $i | cut -f 2 -d ' '`
@@ -19,19 +23,12 @@ function create_new_list() {
 	done
 }
 
-## Establish as empty tempfile
-PKG_TMP=/tmp/.diffapt_tmp
->$PKG_TMP
-
 function compare_apt_list() {
 	cat $1 | while read i; do
 		PKG_STR=`echo $i | cut -f 1 -d ' '`
 		PKG_VER=`echo $i | cut -f 2 -d ' '`
 		if [[ -n `dpkg --list $PKG_STR 2>/dev/null` ]]; then
-			## Create the installed list beforehand, like --create it,
-			## if any occurances of pkg_str are found, remove it
-			## from the list, at the end, this becomes the list of
-			## extra packages!!! :D
+			## Update list of 'extra' packages
 			sed -i '/'$PKG_STR'\ /d' $PKG_TMP
 			USR_VER=`dpkg --list $PKG_STR | tail -n+6 | awk '{print $3}'`
 			if [[ "$PKG_VER" == "$USR_VER" ]]; then
@@ -44,19 +41,16 @@ function compare_apt_list() {
 			echo "Missing package: ${RED}$PKG_STR $PKG_VER${RST}"
 		fi
 	done
-	cat $PKG_TMP > final.txt
-	rm $PKG_TMP
-	EXTRA=`cat final.txt | wc -l`
-	if [[ $EXTRA -gt 0 ]]; then
-		echo "${RED}$EXTRA extra packages installed${RST}"
-		cat final.txt
-		rm final.txt
-	else
-		echo "No extra packages found"
+
+	## Show extra packages
+	PKG_EXT=`cat $PKG_TMP | wc -l`
+	if [[ $PKG_EXT -gt 0 ]]; then
+		echo "${RED}$PKG_EXT extra packages installed:${RST}"
+		cat $PKG_TMP
 	fi
+	rm $PKG_TMP
 }
 
-echo start
 if [[ $F_CREATE -eq 1 ]]; then
 	printf "creating new apt list... "
 	create_new_list new_apt_list.txt
@@ -65,7 +59,7 @@ else
 	printf "creating new apt list..."
 	create_new_list $PKG_TMP
 	printf "done\n"
-	compare_apt_list $1 $2
+	compare_apt_list $1
 fi
 
 exit
